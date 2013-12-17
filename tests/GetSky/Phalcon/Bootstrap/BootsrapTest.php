978e1819d1f4ac7c6bbe6faecaa206c36bc4e176
Alexander.Getmansky[core] <getmansk-y@yandex.ru>
2013-12-17 21:17:17 +0400

    Added  tests for setPatchConfig() and getPathConfig()

---
+++ tests/GetSky/Phalcon/Bootstrap/BootsrapTest.php
@@ -0,0 +1,38 @@
+<?php
+namespace GetSky\Phalcon\AutoloadServices\Tests;
+
+use GetSky\Phalcon\Bootstrap\Bootstrap;
+use Phalcon\Config;
+use Phalcon\DI\FactoryDefault;
+use PHPUnit_Framework_TestCase;
+
+class RegistrantTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var Bootstrap
+     */
+    protected $bootstrap;
+
+    public function testSetGetPathConfig()
+    {
+        $default = $this->bootstrap->getPathConfig();
+        $this->assertSame(Bootstrap::DEFAULT_CONFIG, $default);
+
+        $test = "test.ini";
+        $this->bootstrap->setPathConfig($test);
+        $this->assertSame($test, $this->bootstrap->getPathConfig());
+
+
+    }
+
+    protected function setUp()
+    {
+        $this->bootstrap = new Bootstrap(new FactoryDefault());
+    }
+
+    protected function tearDown()
+    {
+        $this->bootstrap = null;
+    }
+} 
